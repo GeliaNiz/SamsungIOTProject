b9dd5e60ac77d17207619d0310fabaf821ceefde
Maksim <mdroid308@gmail.com>
2021-05-01 22:06:55 +0300

    Added support of multiple pots

--- main_service/urls.py
+++ main_service/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import StateView
+from .views import get_users
 
 urlpatterns = [
-    path('', StateView.as_view())
+    path('', StateView.as_view()),
+    path('get_users/', get_users)
 ]
--- main_service/views.py
+++ main_service/views.py
@@ -1,11 +1,21 @@
 from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 from rest_framework.views import APIView
 from .models import *
 from .serializers import StateSerializer
 
 
 class StateView(APIView):
-    def get(self,request):
+    def get(self, request):
         state = State.objects.all()
         ser_state = StateSerializer(state, many=True)
-        return JsonResponse(ser_state.data,safe=False)
+        context = {'states': ser_state.data}
+        return render(request, 'dashboard.html', context)
+
+
+@require_http_methods(["GET"])
+def get_users(request):
+    state = State.objects.all()
+    ser_state = StateSerializer(state, many=True)
+    return JsonResponse(ser_state.data, safe=False)
