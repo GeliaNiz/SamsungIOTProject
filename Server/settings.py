a04c1a0d9753f4373f39ccce08e0a04e487811b1
GelNiz <angelinanizam@gmail.com>
2021-05-09 00:53:56 +0500

    last

--- Server/settings.py
+++ Server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'PlantDB',
         'USER': 'postgres',
-        'PASSWORD': 'adminka',
+        'PASSWORD': 'sports',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
@@ -124,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+   ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
